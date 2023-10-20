576aa90c8fb8166e1576bb7031d45c13ac33e08a
jcgreaux <dccommit97@gmail.com>
2023-10-19 22:21:35 -0400

    adding truck and segment

--- app.js
+++ app.js
@@ -1,73 +1,106 @@
-const uno = document.getElementById("charge_1")
-const dos = document.getElementById("charge_2")
-const tres = document.getElementById("charge_3")
-const quatro = document.getElementById("charge_4")
-
-let fillUno = 25;
-let fillDos = 25;
-let fillTres = 25;
-let fillQuatro = 25;
-
-function display() {
-	uno.style = `height:${fillUno}%`
-	dos.style = `height:${fillDos}%`
-	tres.style = `height:${fillTres}%`
-	quatro.style = `height:${fillQuatro}%`
-}
-
-const handleUno = ()=>{
-	fillUno += 15
-	fillDos -= fillDos > 0 ? 5 : 0
-	fillTres -= 5
-	fillQuatro -= 5
-	display()
-}
-const handleDos = ()=>{
-	fillUno -= 5
-	fillDos += 15
-	fillTres -= 5
-	fillQuatro -= 5
-	display()
-}
-const handleTres = ()=>{
-	fillUno -= 5
-	fillDos -= 5
-	fillTres += 15
-	fillQuatro -= 5
-	display()
-}
-const handleQuatro = ()=>{
-	fillUno -= 5
-	fillDos -= 5
-	fillTres -= 5
-	fillQuatro += 15
-	display()
-}
-
-display()
-
-//canvas
-
 class Truck {
 	constructor(id) {
 		this.canvas = document.getElementById(id)
 		this.ctx = this.canvas.getContext("2d")
 		this.state = {
+			truck:{
+				cabin:{
+					height:200,
+					width: 160
+				},
+				wheels: {
+					d: 40,
+					pos_av: 100,
+					pos_ar: 400,
+					y:250
+				},
+				bed:{
+					length:300,
+					start: 190,
+					y: 195,
+					height: 10
+				}
+			},
 			charge:{
 				height: 100,
 				width: 300,
-				g: {x:350, y: 150}
+				g: {x:340, y: 130}
 			},
 			tracking: {
 				elmt:"none",
 				currentX:0
 			}
 		}
+		this.build_truck()
 		this.build_charge()
 		this.canvas.addEventListener("mousedown", (e)=> {this.handle_click_down(e)})
 		this.canvas.addEventListener("mouseup", (e)=> {this.handle_click_up(e)})
 		this.canvas.addEventListener("touchstart", (e)=> {this.handle_touch_start(e)})
 		this.canvas.addEventListener("touchend", (e)=> {this.handle_touch_end(e)})
+	}
+	
+	build_truck(){
+		const ctx = this.ctx
+		//wheels
+		const wheels = this.state.truck.wheels
+		//av
+		ctx.beginPath();
+		ctx.arc(wheels.pos_av, wheels.y, wheels.d, 0, 2 * Math.PI)
+		ctx.fillStyle = "black"
+		ctx.fill()
+		ctx.beginPath();
+		ctx.arc(wheels.pos_av, wheels.y, wheels.d/3, 0, 2 * Math.PI)
+		ctx.fillStyle = "white"
+		ctx.fill()
+		//arc
+		ctx.beginPath();
+		ctx.arc(wheels.pos_ar, wheels.y, wheels.d, 0, 2 * Math.PI)
+		ctx.fillStyle = "black"
+		ctx.fill()
+		ctx.beginPath();
+		ctx.arc(wheels.pos_ar, wheels.y, wheels.d/3, 0, 2 * Math.PI)
+		ctx.fillStyle = "white"
+		ctx.fill()
+		//bed
+		const bed = this.state.truck.bed
+		ctx.beginPath()
+		ctx.rect(bed.start, bed.y, bed.length, bed.height)
+		ctx.stroke()
+		//cabin
+		const cabin = this.state.truck.cabin
+		ctx.beginPath()
+		//around the wheel
+		ctx.arc(wheels.pos_av , wheels.y, wheels.d + 5, Math.PI, 2 * Math.PI)
+		//bottom back of the cabin
+		ctx.lineTo(wheels.pos_av + cabin.width/2, wheels.y)
+		//back of the cabin
+		ctx.lineTo(wheels.pos_av + cabin.width/2, wheels.y - cabin.height)
+		//roof of the cabin
+		ctx.quadraticCurveTo(60, 40, wheels.pos_av - cabin.width/2.5, wheels.y - cabin.height)
+		//windshield
+		ctx.quadraticCurveTo(70, 40, wheels.pos_av - cabin.width/2, wheels.y - cabin.height/2)
+		ctx.quadraticCurveTo(30, 60, wheels.pos_av - cabin.width/2.5, wheels.y - cabin.height)
+		//front of the cabin
+		ctx.moveTo(wheels.pos_av - cabin.width/2, wheels.y - cabin.height/2)
+		ctx.lineTo(wheels.pos_av - cabin.width/2, wheels.y)
+		//bottom front of the cabin
+		ctx.lineTo(wheels.pos_av - wheels.d - 5, wheels.y)
+		//door
+		ctx.moveTo(wheels.pos_av - wheels.d - 10, wheels.y - wheels.d - 10)
+		ctx.quadraticCurveTo(60, 200, wheels.pos_av + wheels.d + 10, wheels.y - wheels.d - 10)
+		ctx.quadraticCurveTo(160, 100,wheels.pos_av + wheels.d + 10, wheels.y - cabin.height * 0.9)
+		ctx.quadraticCurveTo(140, 75, wheels.pos_av - wheels.d * 0.7, wheels.y - cabin.height * 0.9)
+		ctx.quadraticCurveTo(40, 110, wheels.pos_av - wheels.d - 10, wheels.y - wheels.d - 10)
+		//window
+		ctx.moveTo(wheels.pos_av - wheels.d * 0.5, wheels.y - cabin.height * 0.85)
+		ctx.quadraticCurveTo(70, 100, wheels.pos_av - wheels.d, wheels.y - cabin.height * 0.5)
+		ctx.quadraticCurveTo(100, 140,wheels.pos_av + wheels.d + 10, wheels.y - cabin.height * 0.5)
+		ctx.quadraticCurveTo(150, 80, wheels.pos_av + wheels.d + 10, wheels.y - cabin.height * 0.85)
+		ctx.quadraticCurveTo(100, 90, wheels.pos_av - wheels.d * 0.5, wheels.y - cabin.height * 0.85)
+		//end
+		ctx.stroke()
+		this.build_charge()
+		this.build_segments()
 	}
 	
 	build_charge(){
@@ -121,6 +154,83 @@ class Truck {
 		
 	}
 	
+	build_segments() {
+		const ctx = this.ctx
+		const wheels = this.state.truck.wheels
+		const g = this.state.charge.g
+		const line_dash = [5, 5]
+		//Y
+		const dist_Y = 80
+		ctx.beginPath()
+		ctx.setLineDash(line_dash)
+		ctx.moveTo(wheels.pos_ar, wheels.y)
+		ctx.lineTo(wheels.pos_ar, wheels.y + dist_Y)
+		ctx.moveTo(g.x, g.y)
+		ctx.lineTo(g.x, wheels.y + dist_Y)
+		ctx.stroke()
+		ctx.beginPath()
+		ctx.setLineDash([0, 0])
+		ctx.moveTo(g.x, wheels.y + dist_Y)
+		ctx.lineTo(wheels.pos_ar, wheels.y + dist_Y);
+		ctx.stroke()
+		ctx.font = "10px Arial"
+		ctx.fillStyle = "black"
+		ctx.fillText("Y", g.x + (wheels.pos_ar - g.x)/2, wheels.y + dist_Y - 5)
+		//Lc
+		const dist_lc = 110
+		const bed = this.state.truck.bed
+		ctx.beginPath()
+		ctx.setLineDash(line_dash)
+		ctx.moveTo(bed.start, bed.y)
+		ctx.lineTo(bed.start, wheels.y + dist_lc)
+		ctx.moveTo(bed.start + bed.length, bed.y)
+		ctx.lineTo(bed.start + bed.length, wheels.y + dist_lc)
+		ctx.stroke()
+		ctx.beginPath()
+		ctx.setLineDash([0, 0])
+		ctx.moveTo(bed.start, wheels.y + dist_lc)
+		ctx.lineTo(bed.start + bed.length, wheels.y + dist_lc);
+		ctx.stroke()
+		ctx.font = "10px Arial"
+		ctx.fillStyle = "black"
+		ctx.fillText("Lc", bed.start + bed.length/2, wheels.y + dist_lc - 5)
+		//F'
+		const dist_f = 130
+		ctx.beginPath()
+		ctx.setLineDash(line_dash)
+		ctx.moveTo(wheels.pos_av, wheels.y)
+		ctx.lineTo(wheels.pos_av, wheels.y + dist_f)
+		ctx.moveTo(wheels.pos_ar, wheels.y)
+		ctx.lineTo(wheels.pos_ar, wheels.y + dist_f)
+		ctx.stroke()
+		ctx.beginPath()
+		ctx.setLineDash([0, 0])
+		ctx.moveTo(wheels.pos_av, wheels.y + dist_f)
+		ctx.lineTo(wheels.pos_ar, wheels.y + dist_f);
+		ctx.stroke()
+		ctx.font = "10px Arial"
+		ctx.fillStyle = "black"
+		ctx.fillText("F'", wheels.pos_av + (wheels.pos_ar - wheels.pos_av)/2, wheels.y + dist_f - 5)
+		//L
+		const dist_l = 160
+		const cabin = this.state.truck.cabin
+		ctx.beginPath()
+		ctx.setLineDash(line_dash)
+		ctx.moveTo(wheels.pos_av - cabin.width/2, wheels.y)
+		ctx.lineTo(wheels.pos_av - cabin.width/2, wheels.y + dist_l)
+		ctx.moveTo(bed.start + bed.length, bed.y)
+		ctx.lineTo(bed.start + bed.length, wheels.y + dist_l)
+		ctx.stroke()
+		ctx.beginPath()
+		ctx.setLineDash([0, 0])
+		ctx.moveTo(wheels.pos_av - cabin.width/2, wheels.y + dist_l)
+		ctx.lineTo(bed.start + bed.length, wheels.y + dist_l);
+		ctx.stroke()
+		ctx.font = "10px Arial"
+		ctx.fillStyle = "black"
+		ctx.fillText("L", wheels.pos_av + (wheels.pos_ar - wheels.pos_av)/2, wheels.y + dist_l - 5)
+	}
+	
 	mouse_track(e, c){
 		const x = e.offsetX
 		const y = e.offsetY
@@ -143,7 +253,6 @@ class Truck {
 		const c = e.target
 		const x = e.offsetX
 		const y = e.offsetY
-		console.log(x,y)
 		let el = "none"
 		if(this.is_left_arrows(x,y)){
 			el = "LA"
@@ -266,7 +375,7 @@ class Truck {
 			default:
 				break
 		}
-		c.build_charge()
+		c.build_truck()
 		setTimeout(()=> c.update_tracking(c), 100)
 	}
 }
